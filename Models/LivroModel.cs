19221aad6f380e7154825fcf367b6cd528c45e09
Thiago Tavares <Thiagoff2019@hotmail.com>
2024-11-28 10:54:56 -0300

    first commit

---
+++ Models/LivroModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiProjeto.Models;
+
+public class LivroModel
+{
+    public int id { get; set; }
+    
+    public string Titulo { get; set; }
+    
+    public AutorModel Autor { get; set; }
+}
